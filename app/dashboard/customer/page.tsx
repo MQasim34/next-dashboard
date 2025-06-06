ab649a515fec7efca45774a0d7e3fb5edae22bd7
M.Qasim <mq30994@gmail.com>
2025-06-06 15:30:23 +0500

    add

---
+++ app/dashboard/customer/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const customer = () => {
+  return (
+    <div>
+      Customer Page
+    </div>
+  )
+}
+
+export default customer
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const dashboard = () => {
+  return (
+    <div>
+      DashBoard Page
+    </div>
+  )
+}
+
+export default dashboard
